e84f088620d911c9b8a2ce1424648c7c4a47d813
Víctor Oliva <olivarra1@gmail.com>
2020-08-02 16:27:49 +0200

    fix patchOperator for new ref detection method.

--- packages/rxjs-traces/src/patchOperator.ts
+++ packages/rxjs-traces/src/patchOperator.ts
@@ -1,9 +1,61 @@
 import { Observable } from 'rxjs';
+import { getMetadata, findReverseTagRefs, detectRefChanges } from './metadata';
 
 export const patchOperator = <
   T extends (...args: any[]) => (source: Observable<any>) => Observable<any>
 >(
   operator: T
 ): T => {
-  return operator;
+  /**
+   * This will control:
+   * - Regular top-to-bottom refernces
+   * - Parameter references (e.g. withLatestFrom(something$))
+   * - inner observable references (e.g. mergeAll)
+   */
+  const resultOperator = (...args: any[]) => {
+    // Get the list of stream arguments to link refs.
+    const streamArgs = args.filter((arg) => arg instanceof Observable);
+
+    return (source: Observable<unknown>) => {
+      // Map arguments to mock out functions and find out if they're projections
+      const mappedArgs = args.map((arg) => {
+        if (typeof arg !== 'function') {
+          return arg;
+        }
+        return (...argFnArgs: any[]) => {
+          const argFnResult = arg(...argFnArgs);
+          // Synchronous calls won't work, because `result` hasn't been created yet
+          if (!(argFnResult instanceof Observable) || !result) {
+            return argFnResult;
+          }
+
+          // It's a projection, we can link refs
+          const dependantTagRefs = findReverseTagRefs(source);
+          detectRefChanges(() => {
+            const resultMetadata = getMetadata(result);
+            resultMetadata.refs.add(argFnResult);
+            getMetadata(argFnResult).reverseRefs.add(result);
+          }, dependantTagRefs);
+          return argFnResult;
+        };
+      });
+
+      const applied = operator(...mappedArgs);
+      const result = applied(source);
+      const resultMetadata = getMetadata(result);
+      const sourceMetadata = getMetadata(source);
+
+      resultMetadata.refs.add(source);
+      sourceMetadata.reverseRefs.add(result);
+
+      streamArgs.forEach((arg) => {
+        const argMetadata = getMetadata(arg);
+        resultMetadata.refs.add(arg);
+        argMetadata.reverseRefs.add(result);
+      });
+      return result;
+    };
+  };
+
+  return resultOperator as T;
 };
--- packages/rxjs-traces/test/patchOperator.test.ts
+++ packages/rxjs-traces/test/patchOperator.test.ts
@@ -1,7 +1,14 @@
 import { Observable, of } from 'rxjs';
-import { delay, filter, map, takeLast, withLatestFrom } from 'rxjs/operators';
+import {
+  delay,
+  filter,
+  map,
+  takeLast,
+  withLatestFrom,
+  mergeMap,
+} from 'rxjs/operators';
 import { addDebugTag, patchObservable, patchOperator, tagValue$ } from '../src';
-import { resetTag$ } from '../src/debugTag';
+import { resetTag$ } from '../src/changes';
 import { restoreObservable } from '../src/patchObservable';
 
 afterEach(() => {
@@ -19,14 +26,15 @@ describe('patchOperator (contrived custom operators)', () => {
   it("doesn't break references of existing operators", async () => {
     const stream = of(1).pipe(
       addDebugTag('source'),
-      patchOperator(map)(v => v + 1),
+      patchOperator(map)((v) => v + 1),
       addDebugTag('middle'),
-      patchOperator(filter)(v => v > 0),
+      patchOperator(filter)((v) => v > 0),
       addDebugTag('result')
     );
     const tags = await stream
       .pipe(
         takeLast(1),
+        delay(0),
         withLatestFrom(tagValue$),
         map(([_, tags]) => tags)
       )
@@ -38,8 +46,8 @@ describe('patchOperator (contrived custom operators)', () => {
   });
 
   it('handles delayed subscriptions', async () => {
-    const operator = patchOperator(() => <T>(source: Observable<T>) =>
-      new Observable(obs => {
+    const delaySubscription = patchOperator(() => <T>(source: Observable<T>) =>
+      new Observable((obs) => {
         setTimeout(() => {
           source.subscribe(obs);
         }, 10);
@@ -47,12 +55,13 @@ describe('patchOperator (contrived custom operators)', () => {
     );
     const stream = of(1).pipe(
       addDebugTag('source'),
-      operator(),
+      delaySubscription(),
       addDebugTag('result')
     );
     const tags = await stream
       .pipe(
         takeLast(1),
+        delay(0),
         withLatestFrom(tagValue$),
         map(([_, tags]) => tags)
       )
@@ -63,8 +72,10 @@ describe('patchOperator (contrived custom operators)', () => {
   });
 
   it('handles references through arguments', async () => {
-    const concat = <T>(...args: Observable<T>[]) => (source: Observable<T>) =>
-      new Observable(obs => {
+    const concatDelay = <T>(...args: Observable<T>[]) => (
+      source: Observable<T>
+    ) =>
+      new Observable((obs) => {
         const observableList = [source, ...args];
         let i = 0;
         const subscribeNext = () => {
@@ -72,16 +83,16 @@ describe('patchOperator (contrived custom operators)', () => {
             return obs.complete();
           }
           observableList[i].subscribe(
-            value => obs.next(value),
-            error => obs.error(error),
-            subscribeNext
+            (value) => obs.next(value),
+            (error) => obs.error(error),
+            () => setTimeout(subscribeNext)
           );
           i++;
         };
         subscribeNext();
       });
 
-    const patchedConcat = patchOperator(concat);
+    const patchedConcat = patchOperator(concatDelay);
 
     const createSource = (id: number) =>
       of(id).pipe(delay(10), addDebugTag('source ' + id));
@@ -93,42 +104,43 @@ describe('patchOperator (contrived custom operators)', () => {
     const tags = await stream
       .pipe(
         takeLast(1),
+        delay(0),
         withLatestFrom(tagValue$),
         map(([_, tags]) => tags)
       )
       .toPromise();
 
-    expect(tags.result.refs).toEqual(['source 0', 'source 1', 'source 2']);
+    expect(tags.result.refs.length).toBe(3);
+    expect(tags.result.refs).toContain('source 0');
+    expect(tags.result.refs).toContain('source 1');
+    expect(tags.result.refs).toContain('source 2');
     expect(tags['source 0'].refs).toEqual([]);
     expect(tags['source 1'].refs).toEqual([]);
     expect(tags['source 2'].refs).toEqual([]);
   });
 
   it('handles references from projection arguments', async () => {
-    /** Q: shouldn't this operator get handled by default? :thinking_face:
-     * A: No - these get detected on operators that use .lift, because we have
-     *     better control on "obs.next" tracking.
-     * We need to consider all cases if we want to track this globally...
-     */
-    const mergeMap = <T, R>(mapFn: (value: T) => Observable<R>) => (
+    const delayedMergeMap = <T, R>(mapFn: (value: T) => Observable<R>) => (
       source: Observable<T>
     ) =>
-      new Observable(obs => {
+      new Observable((obs) => {
         let subscriptions = 1;
         source.subscribe(
-          v => {
+          (v) => {
             subscriptions++;
-            mapFn(v).subscribe(
-              value => obs.next(value),
-              error => obs.error(error),
-              () => {
-                if (--subscriptions === 0) {
-                  obs.complete();
+            setTimeout(() => {
+              mapFn(v).subscribe(
+                (value) => obs.next(value),
+                (error) => obs.error(error),
+                () => {
+                  if (--subscriptions === 0) {
+                    obs.complete();
+                  }
                 }
-              }
-            );
+              );
+            });
           },
-          error => obs.error(error),
+          (error) => obs.error(error),
           () => {
             if (--subscriptions === 0) {
               obs.complete();
@@ -137,22 +149,25 @@ describe('patchOperator (contrived custom operators)', () => {
         );
       });
 
-    const patchedMergeMap = patchOperator(mergeMap);
+    const patchedMergeMap = patchOperator(delayedMergeMap);
 
     const stream = of(1).pipe(
       addDebugTag('source'),
-      patchedMergeMap(v => of(v).pipe(addDebugTag('inner' + v))),
+      patchedMergeMap((v) => of(v).pipe(addDebugTag('inner' + v))),
       addDebugTag('result')
     );
     const tags = await stream
       .pipe(
         takeLast(1),
+        delay(0),
         withLatestFrom(tagValue$),
         map(([_, tags]) => tags)
       )
       .toPromise();
 
-    expect(tags.result.refs).toEqual(['source', 'inner1']);
+    expect(tags.result.refs.length).toBe(2);
+    expect(tags.result.refs).toContain('source');
+    expect(tags.result.refs).toContain('inner1');
     expect(tags.source.refs).toEqual([]);
     expect(tags.inner1.refs).toEqual([]);
   });
